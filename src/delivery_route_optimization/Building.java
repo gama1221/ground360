011c5457e1eeea83fbbf881359816e573a7efd2d
Getinet Amare <getinetamare@gmail.com>
2023-02-15 10:44:23 +0300

    Add Building java class

---
+++ src/delivery_route_optimization/Building.java
@@ -0,0 +1,63 @@
+package delivery_route_optimization;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Building {
+    /**
+     * Building properties
+     * Setter and Getter methods
+     * Used to access private fields by other classes
+     */
+    private String name;
+    private LinkedList<Building> shortestPath = new LinkedList<>();
+    private Integer distance = Integer.MAX_VALUE;// considers as infinity value except to the starting building
+    private Map<Building, Integer> adjacentBuilding = new HashMap<>();
+
+    /**
+     * A constructor for creating Building object
+     * @param name - the param used to create Building object via name parameter
+     */
+    public Building(String name){
+        this.name = name;
+    }
+    /**
+     * Attach and map the building with distance
+     */
+    public void addDestination(Building destination, int distance){
+        adjacentBuilding.put(destination, distance);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LinkedList<Building> getShortestPath() {
+        return shortestPath;
+    }
+
+    public void setShortestPath(LinkedList<Building> shortestPath) {
+        this.shortestPath = shortestPath;
+    }
+
+    public Integer getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    public Map<Building, Integer> getAdjacentBuilding() {
+        return adjacentBuilding;
+    }
+
+    public void setAdjacentBuilding(Map<Building, Integer> adjacentBuilding) {
+        this.adjacentBuilding = adjacentBuilding;
+    }
+}
