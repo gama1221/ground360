563fa980f7e67de4a5cc6c410159406196cdfc2c
Getinet Amare <getinetamare@gmail.com>
2023-02-15 17:29:24 +0300

    Modify a router]

--- src/main/java/com/shortestroute/ground360/Ground360Application.java
+++ src/main/java/com/shortestroute/ground360/Ground360Application.java
@@ -3,9 +3,6 @@ package com.shortestroute.ground360;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Objects;
-import java.util.stream.Collectors;
-
 @SpringBootApplication
 public class Ground360Application {
     public static void main(String[] args) {
