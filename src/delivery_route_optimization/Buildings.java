ec30279ae33bc16672ece6866954648d15d14ab4
Getinet Amare <getinetamare@gmail.com>
2023-02-15 10:44:47 +0300

    Add Buildings java class

---
+++ src/delivery_route_optimization/Buildings.java
@@ -0,0 +1,30 @@
+package delivery_route_optimization;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Buildings {
+    /**
+     * Contains Building properties as well as add building method to buildingSet collection object
+     * Buildings are the collection of building i.e. one or more building can make Buildings properties
+     * Setter and Getter methods
+     * Used to access private fields by other classes
+     */
+    private Set<Building> buildingSet = new HashSet<>();
+
+    /**
+     * Add a building to set of buildings object
+     * @param building - an individual building to be added to set of buildings
+     */
+    public void addBuilding(Building building){
+        buildingSet.add(building);
+    }
+
+    public Set<Building> getBuildingSet() {
+        return buildingSet;
+    }
+
+    public void setBuildingSet(Set<Building> buildingSet) {
+        this.buildingSet = buildingSet;
+    }
+}
